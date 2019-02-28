07eb4c5442388604d876eb62d68e542e82a73ef6
Nikhil Kilivayil <nikhilkilivayil@gmail.com>
2019-02-28 13:33:59 +0530

    NavBar design added

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs=require('hbs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+hbs.registerPartials(__dirname+'/views/partials');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
